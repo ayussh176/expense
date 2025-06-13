d727643dc02d26c3c093024553654224ca1cfaa6
ayussh176 <ayushmalik852@gmail.com>
2025-06-13 11:02:53 +0530

    html change

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { ExpenseProvider } from "./contexts/ExpenseContext";
-import { ThemeProvider } from "./contexts/ThemeContext"; // ✅ import ThemeProvider
+import { ThemeProvider } from "./contexts/ThemeContext"; 
 import ProtectedRoute from "./components/ProtectedRoute";
 import Navbar from "./components/Navbar";
 import Index from "./pages/Index";
@@ -25,7 +25,7 @@ const App = () => (
       <Sonner />
       <AuthProvider>
         <ExpenseProvider>
-          <ThemeProvider> {/* ✅ Wrap with ThemeProvider */}
+          <ThemeProvider> 
             <BrowserRouter>
               <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800 transition-colors duration-300">
                 <Routes>
